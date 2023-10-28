9875bc13dc546c5d778eaa11de367ca6f815849e
amrita-pasupuleti <130122797+amrita-pasupuleti@users.noreply.github.com>
2023-10-28 14:02:11 -0700

    Update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import "./Home-style.css";
 
 const Home = () => {
   return (
-    <>
+    <div className="container">
       <div className="sidebar">
         <h2>Sections</h2>
         <ul>
@@ -28,46 +28,73 @@ const Home = () => {
       <div className="content">
         <section id="first">
           <h1>About Me</h1>
-          <p>
+          <p className="AboutContent">
             Hi, my name is Amrita Pasupuleti! I'm a second-year computer science
-            major at the University of California, Santa Cruz.
+            major at the University of California, Santa Cruz. I like to read
+            and play video games in my free time. I am always looking forward to
+            acquiring more skills and learning everything I can!
           </p>
         </section>
 
         <section id="second">
           <h1>Skills</h1>
           <ul className="skills-list">
-            <li>Web Design: HTML, css, JavaScript, React</li>
-            <li>Coding Languages: Python, C, Java</li>
-            <li>Other: </li>
+            <li>Python</li>
+            <li>Java</li>
+            <li>C</li>
+            <li>JavaScript, React</li>
+            <li>HTML, CSS</li>
           </ul>
         </section>
 
         <section id="third">
           <h1>Contacts</h1>
-          <p>Email: amritapasupuleti@gmail.com</p>
-          <p>Phone: +1(510)303-2490</p>
-          <p>Discord: am1r1t3</p>
+          <ul className="contactsList">
+            <li>
+              <p>Email:</p>
+              <a className="links" href="amritapasupuleti@gmail.com">
+                amritapasupuleti@gmail.com
+              </a>
+            </li>
+            <li>
+              <p>Phone: </p>
+              <p>+1(510)303-2490</p>
+            </li>
+            <li>
+              <p>Discord:</p>
+              <p>am1r1t3</p>
+            </li>
+          </ul>
         </section>
 
         <section id="fourth">
           <h1>Links</h1>
-          <p>LinkedIn:</p>
-          <p>
-            <a
-              href="https://www.linkedin.com/in/amrita-pasupuleti-a74945291/"
-              target="blank"
-            >
-              https://www.linkedin.com/in/amrita-pasupuleti-a74945291/
-            </a>
-          </p>
-          <br></br>
-          <p>GitHub:</p>
-          <p>
-            <a href="https://github.com/amrita-pasupuleti" target="blank">
-              https://github.com/amrita-pasupuleti{" "}
-            </a>{" "}
-          </p>
+          <ul className="contactsList">
+            <li>
+              <p>LinkedIn:</p>
+              <p>
+                <a
+                  className="links"
+                  href="https://www.linkedin.com/in/amrita-pasupuleti-a74945291/"
+                  target="blank"
+                >
+                  https://www.linkedin.com/in/amrita-pasupuleti-a74945291/
+                </a>
+              </p>
+            </li>
+            <li>
+              <p>GitHub:</p>
+              <p>
+                <a
+                  className="links"
+                  href="https://github.com/amrita-pasupuleti"
+                  target="blank"
+                >
+                  https://github.com/amrita-pasupuleti
+                </a>
+              </p>
+            </li>
+          </ul>
         </section>
 
         <section id="fifth">
@@ -80,7 +107,11 @@ const Home = () => {
           <p>Any other information or details you'd like to share.</p>
         </section>
       </div>
-    </>
+
+      <div className="right-sidebar">
+        <p>Check back later for more content:)</p>
+      </div>
+    </div>
   );
 };
 
