672da0a094565aec4117e406e2b6c1d273450f5e
Amrita Pasupuleti <amritapasupuleti@gmail.com>
2025-06-19 19:26:20 -0500

    Experience update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,9 +10,9 @@ export const About = () => {
         <h1 className={styles.title}>Amrita Pasupuleti</h1>
         <p className={styles.description}>
           I'm a third year Computer Science student at the University of
-          California, Santa Cruz. I am currently working as a computer science,
-          and recently completed a machine learning internship. I also enjoyed
-          being an AI researcher at UCSC, in addition to helping build full
+          California, Santa Cruz. I am currently working as a software engineer
+          intern at Capital One. I also enjoyed being an AI researcher and
+          Computer Science Tutor at UCSC, in addition to helping build full
           stack applications for various projects.
         </p>
       </div>
